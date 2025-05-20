8a19acbcac174b0ee5249bd67ef3a47076d3c2db
Micaela Aimone <mica.aimone@gmail.com>
2025-05-20 18:28:38 -0300

    creamos repository para cada entidad

--- src/main/java/com/example/demo/model/repositories/PeliculaRepository.java
+++ src/main/java/com/example/demo/model/repositories/PeliculaRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.model.repositories;
 
 import com.example.demo.model.entities.ContenidoEntity;
+import com.example.demo.model.entities.PeliculaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContenidoRepository extends JpaRepository<ContenidoEntity, Long> {
-
+public interface PeliculaRepository extends JpaRepository<PeliculaEntity, Long> {
 }
--- src/main/java/com/example/demo/model/repositories/RatingRepository.java
+++ src/main/java/com/example/demo/model/repositories/RatingRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.model.repositories;
 
 import com.example.demo.model.entities.ContenidoEntity;
+import com.example.demo.model.entities.RatingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContenidoRepository extends JpaRepository<ContenidoEntity, Long> {
-
+public interface RatingRepository extends JpaRepository<RatingEntity, Long> {
 }
