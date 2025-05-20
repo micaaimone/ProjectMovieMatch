d057d1036f6a9aa49e842936e3bf9979b066715b
Micaela Aimone <mica.aimone@gmail.com>
2025-05-20 11:19:02 -0300

    first commit

---
+++ src/main/java/com/example/demo/ProyectoMovieMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMovieMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMovieMatchApplication.class, args);
+	}
+
+}
