1cb7137fff87b05d8b5cf9b314008f098b6dc204
Micaela Aimone <mica.aimone@gmail.com>
2025-05-20 11:44:10 -0300

    agregamos propierties y base para el proyecto

--- src/main/java/com/example/demo/ProyectoMovieMatchApplication.java
+++ src/main/java/com/example/demo/ProyectoMovieMatchApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoMovieMatchApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("API_KEY",
+				dotenv.get("API_KEY"));
+
+		System.setProperty("MERCADOPAGO_ACCESS_TOKEN",
+				dotenv.get("MERCADOPAGO_ACCESS_TOKEN"));
+
 		SpringApplication.run(ProyectoMovieMatchApplication.class, args);
-	}
+
+		String port = dotenv.get("PORT");
+		System.out.println("Puerto: " + port);	}
 
 }
