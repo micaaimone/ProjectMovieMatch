13948dc5f950d0d4384775386c6b246e7d3c82e9
Micaela Aimone <mica.aimone@gmail.com>
2025-05-21 09:42:33 -0300

    modificamos controller para que aplique los nuevos services

--- src/main/java/com/example/demo/model/controllers/ContenidoController.java
+++ src/main/java/com/example/demo/model/controllers/ContenidoController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.DTOs.ContenidoDTO;
 import com.example.demo.model.DTOs.PeliculaDTO;
 import com.example.demo.model.DTOs.SerieDTO;
 import com.example.demo.model.services.ContenidoService;
+import com.example.demo.model.services.PeliculaService;
+import com.example.demo.model.services.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,10 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContenidoController {
 
     private final ContenidoService contenidoService;
+    private final PeliculaService peliculaService;
+    private final SerieService serieService;
 
     @Autowired
-    public ContenidoController( ContenidoService contenidoService) {
+    public ContenidoController(ContenidoService contenidoService, PeliculaService peliculaService, SerieService serieService) {
         this.contenidoService = contenidoService;
+        this.peliculaService = peliculaService;
+        this.serieService = serieService;
     }
 
 
@@ -28,14 +34,14 @@ public class ContenidoController {
     public Page<SerieDTO> listarSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size)
     {
         Pageable pageable = PageRequest.of(page, size);
-        return contenidoService.datosSerieBDD(pageable);
+        return serieService.datosSerieBDD(pageable);
     }
 
     @GetMapping("/verPeliculas")
     public Page<PeliculaDTO> listarPeliculas(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size)
     {
         Pageable pageable = PageRequest.of(page, size);
-        return contenidoService.datosPeliculaBDD(pageable);
+        return peliculaService.datosPeliculaBDD(pageable);
     }
 
     @GetMapping("/verSeriesYPeliculas")
