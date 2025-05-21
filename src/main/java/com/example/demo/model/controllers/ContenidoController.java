3f74c228239de05e9811fea5c9678a74beeb98c2
Micaela Aimone <mica.aimone@gmail.com>
2025-05-20 21:51:13 -0300

    mejoras en el controller. paso al uso de DTOs y pages

--- src/main/java/com/example/demo/model/controllers/ContenidoController.java
+++ src/main/java/com/example/demo/model/controllers/ContenidoController.java
@@ -1,22 +1,17 @@
 package com.example.demo.model.controllers;
 
-import com.example.demo.model.entities.ContenidoEntity;
-import com.example.demo.model.entities.PeliculaEntity;
-import com.example.demo.model.entities.SerieEntity;
-import com.example.demo.model.services.APIMovieService;
+import com.example.demo.model.DTOs.ContenidoDTO;
+import com.example.demo.model.DTOs.PeliculaDTO;
+import com.example.demo.model.DTOs.SerieDTO;
 import com.example.demo.model.services.ContenidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ContenidoController {
@@ -30,21 +25,21 @@ public class ContenidoController {
 
 
     @GetMapping("/verSeries")
-    public Page<SerieEntity> listarSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "2") int size)
+    public Page<SerieDTO> listarSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size)
     {
         Pageable pageable = PageRequest.of(page, size);
         return contenidoService.datosSerieBDD(pageable);
     }
 
     @GetMapping("/verPeliculas")
-    public Page<PeliculaEntity> listarPeliculas(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "2") int size)
+    public Page<PeliculaDTO> listarPeliculas(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size)
     {
         Pageable pageable = PageRequest.of(page, size);
         return contenidoService.datosPeliculaBDD(pageable);
     }
 
     @GetMapping("/verSeriesYPeliculas")
-    public Page<ContenidoEntity> ListarPeliculasYSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size)
+    public Page<ContenidoDTO> ListarPeliculasYSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size)
     {
         Pageable pageable = PageRequest.of(page, size);
         return contenidoService.datosBDD(pageable);
