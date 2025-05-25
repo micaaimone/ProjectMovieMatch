0f24305d2377caaa786c247180f57305e5f31237
bymica <bbrisamorano@gmail.com>
2025-05-25 17:15:55 -0300

    no funca lombok

--- src/main/java/com/example/demo/model/DTOs/UsuarioDTO.java
+++ src/main/java/com/example/demo/model/DTOs/UsuarioDTO.java
@@ -2,21 +2,62 @@ package com.example.demo.model.DTOs;
 
 import com.example.demo.model.entities.ContenidoEntity;
 import com.example.demo.model.entities.CredencialEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 import java.util.Set;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class UsuarioDTO {
     private String username;
     private String email;
     private CredencialEntity credencial;
     //  private SuscripcionEntity suscripcion;
     private Set<ContenidoEntity> likes;
+
+    public UsuarioDTO(String username, String email, CredencialEntity credencial, Set<ContenidoEntity> likes) {
+        this.username = username;
+        this.email = email;
+        this.credencial = credencial;
+        this.likes = likes;
+    }
+
+    public UsuarioDTO() {
+    }
+
+    // GETTERS Y SETTERS
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public CredencialEntity getCredencial() {
+        return credencial;
+    }
+
+    public void setCredencial(CredencialEntity credencial) {
+        this.credencial = credencial;
+    }
+
+    public Set<ContenidoEntity> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<ContenidoEntity> likes) {
+        this.likes = likes;
+    }
 }
--- src/main/java/com/example/demo/model/entities/UsuarioEntity.java
+++ src/main/java/com/example/demo/model/entities/UsuarioEntity.java
@@ -3,19 +3,17 @@ package com.example.demo.model.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import lombok.*;
 
 import java.util.Set;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+//@NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class UsuarioEntity {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -64,5 +62,110 @@ public class UsuarioEntity {
     )
     private Set<ContenidoEntity> likes;
 
+    public UsuarioEntity(Long id, String nombre, String apellido, String email, int edad, long telefono, String contrasenia, String username, boolean activo, CredencialEntity credencial) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.contrasenia = contrasenia;
+        this.username = username;
+        this.activo = activo;
+        this.credencial = credencial;
+        this.likes = likes;
+    }
 
+    public UsuarioEntity() {
+    }
+
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public @Size(min = 6, max = 30) String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(@Size(min = 6, max = 30) String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public @Size(min = 4) String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@Size(min = 4) String username) {
+        this.username = username;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public CredencialEntity getCredencial() {
+        return credencial;
+    }
+
+    public void setCredencial(CredencialEntity credencial) {
+        this.credencial = credencial;
+    }
+
+    public Set<ContenidoEntity> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<ContenidoEntity> likes) {
+        this.likes = likes;
+    }
 }
