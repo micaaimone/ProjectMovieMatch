c229fa69dcf62e722393eb023f5ae3f137f9a808
Micaela Aimone <mica.aimone@gmail.com>
2025-05-27 11:48:51 -0300

    eliminamos comentarios, incluimos puntuacion (general) en la entity

--- src/main/java/com/example/demo/model/mappers/Contenido/PeliculaMapper.java
+++ src/main/java/com/example/demo/model/mappers/Contenido/PeliculaMapper.java
@@ -23,33 +23,8 @@ public class PeliculaMapper {
         PeliculaDTO dto = modelMapper.map(peliculaEntity, PeliculaDTO.class);
 
         if (peliculaEntity.getRatings() != null) {
-//            List<RatingDTO> ratingDTOs = peliculaEntity.getRatings()
-//                    .stream()
-//                    .map(rating -> modelMapper.map(rating, RatingDTO.class))
-//                    .toList();
-//
-//            dto.setRatings(ratingDTOs);
 
-            double promedio = peliculaEntity.getRatings()
-                    .stream()
-                    .mapToDouble(r -> {
-                        String valor = r.getValor();
-                        if (valor == null || valor.isEmpty() || valor.equals("N/A")) {
-                            return 0.0;
-                        }
-                        if (valor.contains("/")) {
-                            String[] parts = valor.split("/");
-                            return Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]) * 10;
-                        } else if (valor.contains("%")) {
-                            return Double.parseDouble(valor.replace("%", "")) / 10;
-                        } else {
-                            return Double.parseDouble(valor);
-                        }
-                    })
-                    .average()
-                    .orElseThrow(() -> new NoSuchElementException("No hay ratings disponibles"));
-
-            dto.setPuntuacionApi(promedio);
+            dto.setPuntuacionApi(peliculaEntity.getPuntuacion());
         }
 
 
@@ -59,15 +34,6 @@ public class PeliculaMapper {
     public PeliculaEntity convertToEntity(PeliculaDTO peliculaDTO)
     {
         PeliculaEntity entity = modelMapper.map(peliculaDTO, PeliculaEntity.class);
-
-//        if (peliculaDTO.getRatings() != null) {
-//            List<RatingEntity> ratingEntities = peliculaDTO.getRatings()
-//                    .stream()
-//                    .map(ratingDTO -> modelMapper.map(ratingDTO, RatingEntity.class))
-//                    .toList();
-//
-//            entity.setRatings(ratingEntities);
-//        }
 
         return entity;
     }
