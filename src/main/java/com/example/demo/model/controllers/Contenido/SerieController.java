cc0de889df4664ea8af253779333790610b71ed5
Micaela Aimone <mica.aimone@gmail.com>
2025-05-26 23:08:18 -0300

    aplicamos nuevos metodos

---
+++ src/main/java/com/example/demo/model/controllers/Contenido/SerieController.java
@@ -0,0 +1,34 @@
+package com.example.demo.model.controllers.Contenido;
+
+import com.example.demo.model.DTOs.Contenido.PeliculaDTO;
+import com.example.demo.model.DTOs.Contenido.SerieDTO;
+import com.example.demo.model.services.Contenido.SerieService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/series")
+public class SerieController {
+    private final SerieService serieService;
+
+    public SerieController(SerieService serieService) {
+        this.serieService = serieService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<SerieDTO>> listarSeries(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size)
+    {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(serieService.datosBDD(pageable));
+    }
+
+    @GetMapping("/buscarByID/{id}")
+    public ResponseEntity<SerieDTO> findByID(@PathVariable Long id)
+    {
+        return ResponseEntity.ok(serieService.buscarByID(id));
+    }
+}
