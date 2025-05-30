6fcf929bb2ce82bc2e496d446b51bfefddf79164
Micaela Aimone <mica.aimone@gmail.com>
2025-05-30 00:20:04 -0300

    cambios en los mappeos

--- src/main/java/com/example/demo/model/mappers/Contenido/PeliculaMapper.java
+++ src/main/java/com/example/demo/model/mappers/Contenido/PeliculaMapper.java
@@ -2,29 +2,36 @@ package com.example.demo.model.mappers.Contenido;
 
 
 import com.example.demo.model.DTOs.Contenido.PeliculaDTO;
-import com.example.demo.model.DTOs.Contenido.RatingDTO;
+import com.example.demo.model.DTOs.ReseñaDTO;
 import com.example.demo.model.entities.Contenido.PeliculaEntity;
-import com.example.demo.model.entities.Contenido.RatingEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Component
 public class PeliculaMapper {
 
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private ReseñaMapper reseñaMapper;
 
     public PeliculaDTO convertToDTO(PeliculaEntity peliculaEntity)
     {
         PeliculaDTO dto = modelMapper.map(peliculaEntity, PeliculaDTO.class);
 
-        if (peliculaEntity.getRatings() != null) {
+        dto.setPuntuacionApi(peliculaEntity.getPuntuacion());
 
-            dto.setPuntuacionApi(peliculaEntity.getPuntuacion());
+        if (peliculaEntity.getReseña() != null)
+        {
+            List<ReseñaDTO> reseñasDTO = peliculaEntity.getReseña()
+                    .stream()
+                    .map(reseñaMapper::convertToDTO)
+                    .collect(Collectors.toList());
+            dto.setReseña(reseñasDTO);
         }
 
 
--- src/main/java/com/example/demo/model/mappers/Contenido/SerieMapper.java
+++ src/main/java/com/example/demo/model/mappers/Contenido/SerieMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.model.mappers.Contenido;
 
 import com.example.demo.model.DTOs.Contenido.RatingDTO;
 import com.example.demo.model.DTOs.Contenido.SerieDTO;
+import com.example.demo.model.DTOs.ReseñaDTO;
 import com.example.demo.model.entities.Contenido.RatingEntity;
 import com.example.demo.model.entities.Contenido.SerieEntity;
 import org.modelmapper.ModelMapper;
@@ -10,20 +11,28 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Component
 public class SerieMapper {
 
     @Autowired
     private ModelMapper modelMapper;
+    private ReseñaMapper reseñaMapper;
 
     public SerieDTO convertToDTO(SerieEntity serieEntity)
     {
         SerieDTO dto = modelMapper.map(serieEntity, SerieDTO.class);
 
-        if (serieEntity.getRatings() != null) {
+        dto.setPuntuacionApi(serieEntity.getPuntuacion());
 
-            dto.setPuntuacionApi(serieEntity.getPuntuacion());
+        if (serieEntity.getReseña() != null)
+        {
+            List<ReseñaDTO> reseñasDTO = serieEntity.getReseña()
+                    .stream()
+                    .map(reseñaMapper::convertToDTO)
+                    .collect(Collectors.toList());
+            dto.setReseña(reseñasDTO);
         }
 
         return dto;
