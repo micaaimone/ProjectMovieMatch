82714fb6daca5d31ab00c1c027ba296bd0f2b92d
Micaela Aimone <mica.aimone@gmail.com>
2025-05-30 22:10:07 -0300

    los movimos d carpeta

--- src/main/java/com/example/demo/model/repositories/Contenido/ReseñaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model.repositories.Contenido;
-
-import com.example.demo.model.entities.ReseñaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReseñaRepository extends JpaRepository<ReseñaEntity, Long> {
-}
