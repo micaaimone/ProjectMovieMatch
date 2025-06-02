804e3c1583fffbac4bc390c89e6350e0a3aef42c
Micaela Aimone <mica.aimone@gmail.com>
2025-06-02 15:19:04 -0300

    nuevo dto

---
+++ src/main/java/com/example/demo/model/DTOs/Contenido/ContenidoMostrarAdminDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.DTOs.Contenido;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ContenidoMostrarAdminDTO {
+    private String titulo;
+    private String tipo;
+}
