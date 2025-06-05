b678fb797a4454277f283355b39e4cc46b0da671
Micaela Aimone <mica.aimone@gmail.com>
2025-06-04 22:40:18 -0300

    borramos comentarios

--- src/main/java/com/example/demo/model/repositories/Contenido/ReseniaRepository.java
+++ src/main/java/com/example/demo/model/repositories/Contenido/ReseniaRepository.java
@@ -11,12 +11,6 @@ import java.util.Optional;
 
 public interface ReseniaRepository extends JpaRepository<ReseniaEntity, Long> {
 
-//    @Query("SELECT r FROM ReseniaEntity r WHERE r.usuario.id = :id_usuario AND r.contenido.id_contenido = :id_contenido AND r.usuario.activo = true AND r.contenido.activo = true")
-//    Optional<ReseniaEntity> findByIDAndContenido(@Param("id_usuario") Long id_usuario, @Param("id_contenido") Long id_contenido);
-//
-//    @Query("SELECT r FROM ReseniaEntity r WHERE r.usuario.id = :id_usuario")
-//    Page<ReseniaEntity> findAllById(Long id_usuario, Pageable pageable);
-
     @Query("SELECT r FROM reseña r WHERE r.usuario.id = :id_usuario AND r.contenido.id_contenido = :id_contenido")
     Optional<ReseniaEntity> findByIDAndContenido(@Param("id_usuario") Long id_usuario, @Param("id_contenido") Long id_contenido);
 
