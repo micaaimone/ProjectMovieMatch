a533b174077dec81d739d68e5dc81ab5325a816d
Micaela Aimone <mica.aimone@gmail.com>
2025-06-11 16:11:49 -0300

    creamos DTO para mostrarle al usuario los datos necesarios de su amigo

---
+++ src/main/java/com/example/demo/model/DTOs/user/AmigoDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.DTOs.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AmigoDTO {
+    private Long id;
+    private String username;
+}
