419907a8e939b35d1c2bc41c09c2be860d27ba74
Micaela Aimone <mica.aimone@gmail.com>
2025-06-11 16:15:08 -0300

    agregamos validaciones

--- src/main/java/com/example/demo/model/DTOs/NewSolicitudAmistadDTO.java
+++ src/main/java/com/example/demo/model/DTOs/NewSolicitudAmistadDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.model.DTOs;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -12,9 +13,11 @@ import lombok.*;
 public class NewSolicitudAmistadDTO {
 
     @NotNull(message = "El id emisor de ser ingresado")
+    @Positive
     private Long idEmisor;
 
     @NotNull(message = "El id receptor de ser ingresado")
+    @Positive
     private Long idReceptor;
 
 }
--- src/main/java/com/example/demo/model/DTOs/SolicitudAmistadDTO.java
+++ src/main/java/com/example/demo/model/DTOs/SolicitudAmistadDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.model.DTOs;
 
 import com.example.demo.model.enums.EstadoSolicitud;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -13,6 +14,7 @@ import lombok.*;
 public class SolicitudAmistadDTO {
 
     @NotNull(message = "El id de ser ingresado")
+    @Positive
     private Long idReceptor;
 
     private EstadoSolicitud estadoSolicitud;
