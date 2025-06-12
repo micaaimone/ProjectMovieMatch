f5a58dd8846381c9b8b0978a363ce74259053b58
Micaela Aimone <mica.aimone@gmail.com>
2025-06-12 19:05:11 -0300

    modificamos controller al separarlo con like controller

--- src/main/java/com/example/demo/model/controllers/user/UsuarioController.java
+++ src/main/java/com/example/demo/model/controllers/user/UsuarioController.java
@@ -1,13 +1,8 @@
 package com.example.demo.model.controllers.user;
 
-import com.example.demo.model.DTOs.Contenido.ContenidoMostrarDTO;
 import com.example.demo.model.DTOs.user.NewUsuarioDTO;
 import com.example.demo.model.DTOs.user.UsuarioDTO;
 import com.example.demo.model.DTOs.user.UsuarioModificarDTO;
-import com.example.demo.model.entities.User.ContenidoLike;
-import com.example.demo.model.entities.User.ReseniaLike;
-import com.example.demo.model.services.Usuarios.ContenidoLikeService;
-import com.example.demo.model.services.Usuarios.ReseniaLikeService;
 import com.example.demo.model.services.Usuarios.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -17,9 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -29,13 +22,9 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    private final ContenidoLikeService contenidoLikeService;
-    private final ReseniaLikeService reseniaLikeService;
 
-    public UsuarioController(UsuarioService usuarioService, ContenidoLikeService contenidoLikeService, ReseniaLikeService reseniaLikeService) {
+    public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-        this.contenidoLikeService = contenidoLikeService;
-        this.reseniaLikeService = reseniaLikeService;
     }
 
     // ------------------- Registro de usuario
@@ -78,8 +67,8 @@ public class UsuarioController {
             @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
     })
     @PreAuthorize("hasAuthority('USUARIO_MODIFICAR')")
-    @PutMapping("/actualizar/{id}")
-    public ResponseEntity<String> actualizarUsuario(
+    @PutMapping("/modificar/{id}")
+    public ResponseEntity<String> modificarUsuario(
             @PathVariable Long id,
             @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     description = "Datos a modificar del usuario",
@@ -106,83 +95,6 @@ public class UsuarioController {
     public ResponseEntity<String> desactivarUsuario(@PathVariable Long id) {
         usuarioService.cambiarEstadoUsuario(id, false);
         return ResponseEntity.ok("Usuario desactivado.");
-    }
-
-    // ------------------- Likes a contenido
-    @Operation(summary = "Dar like a contenido")
-    @PreAuthorize("hasAuthority('USUARIO_LIKE')")
-    @PostMapping("/{usuarioId}/like/contenido/{contenidoId}")
-    public ResponseEntity<String> likeContenido(@PathVariable Long usuarioId, @PathVariable Long contenidoId) {
-        contenidoLikeService.darLike(usuarioId, contenidoId);
-        return ResponseEntity.ok("Like a contenido registrado");
-    }
-
-    @Operation(summary = "Quitar like a contenido")
-    @PreAuthorize("hasAuthority('USUARIO_QUITAR_LIKE')")
-    @DeleteMapping("/{usuarioId}/like/contenido/{contenidoId}")
-    public ResponseEntity<String> quitarLikeContenido(@PathVariable Long usuarioId, @PathVariable Long contenidoId) {
-        boolean eliminado = contenidoLikeService.quitarLike(usuarioId, contenidoId);
-        if (eliminado) {
-            return ResponseEntity.ok("Like a contenido eliminado");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el like para eliminar");
-        }
-    }
-
-    @Operation(summary = "Ver likes de contenido")
-    @PreAuthorize("hasAuthority('USUARIO_VER_LIKES')")
-    @GetMapping("/{usuarioId}/likes/contenido")
-    public ResponseEntity<Page<ContenidoLike>> getContenidoLikes(
-            @PathVariable Long usuarioId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        Page<ContenidoLike> likes = contenidoLikeService.obtenerLikes(usuarioId, page, size);
-        return ResponseEntity.ok(likes);
-    }
-
-    // ------------------- Likes a reseñas
-    @Operation(summary = "Dar like a reseña")
-    @PreAuthorize("hasAuthority('USUARIO_LIKE')")
-    @PostMapping("/{usuarioId}/like/resenia/{reseniaId}")
-    public ResponseEntity<String> likeResenia(@PathVariable Long usuarioId, @PathVariable Long reseniaId) {
-        reseniaLikeService.darLike(usuarioId, reseniaId);
-        return ResponseEntity.ok("Like a reseña registrado");
-    }
-
-    @Operation(summary = "Quitar like a reseña")
-    @PreAuthorize("hasAuthority('USUARIO_QUITAR_LIKE')")
-    @DeleteMapping("/{usuarioId}/like/resenia/{reseniaId}")
-    public ResponseEntity<String> quitarLikeResenia(@PathVariable Long usuarioId, @PathVariable Long reseniaId) {
-        boolean eliminado = reseniaLikeService.quitarLike(usuarioId, reseniaId);
-        if (eliminado) {
-            return ResponseEntity.ok("Like a reseña eliminado");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el like para eliminar");
-        }
-    }
-
-    @Operation(summary = "Ver likes de reseñas")
-    @PreAuthorize("hasAuthority('USUARIO_VER_LIKES')")
-    @GetMapping("/{usuarioId}/likes/resenia")
-    public ResponseEntity<Page<ReseniaLike>> getReseniaLikes(
-            @PathVariable Long usuarioId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        Page<ReseniaLike> likes = reseniaLikeService.obtenerLikes(usuarioId, page, size);
-        return ResponseEntity.ok(likes);
-    }
-
-    // ------------------- Listar contenido con likes
-    @Operation(summary = "Ver contenidos con like")
-    @PreAuthorize("hasAuthority('USUARIO_VER_LIKES')")
-    @GetMapping("/{idUsuario}/likes")
-    public ResponseEntity<Page<ContenidoMostrarDTO>> obtenerLikes(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @PathVariable Long idUsuario) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<ContenidoMostrarDTO> pagina = usuarioService.obtenerLikes(idUsuario, pageable);
-        return ResponseEntity.ok(pagina);
     }
 
     // ------------------- Listar usuarios activos / desactivados
