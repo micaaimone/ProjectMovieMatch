02cad2dbea12a8641d0380e8709912c9f9cba9c9
Micaela Aimone <mica.aimone@gmail.com>
2025-06-14 21:52:20 -0300

    agregamos el metodo para el ignore case

--- src/main/java/com/example/demo/model/enums/Genero.java
+++ src/main/java/com/example/demo/model/enums/Genero.java
@@ -1,8 +1,28 @@
 package com.example.demo.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum Genero {
     Action, Adventure, Animation, Biography, Comedy,
     Crime, Documentary, Drama, Family, Fantasy, History,
     Horror, Music, Musical, Mystery, Romance ,SciFi,
-    Sport,Thriller,War, Western
+    Sport,Thriller,War, Western;
+
+    @JsonCreator
+    public static Genero fromValue(String value) {
+        for (Genero genero : Genero.values()) {
+            if (genero.name().equalsIgnoreCase(value)) {
+                return genero;
+            }
+        }
+        String opcionesDisponibles = Arrays.stream(Genero.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", "));
+
+        throw new IllegalArgumentException("Género inválido: '" + value
+                + "'. Los valores válidos son: " + opcionesDisponibles);
+    }
 }
