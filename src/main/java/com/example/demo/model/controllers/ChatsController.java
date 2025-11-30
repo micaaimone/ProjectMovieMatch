87a6fe77c51e89352d1f071dd296ad9c63463ac5
micaaimone <mica.aimone@gmail.com>
2025-11-30 19:42:28 -0300

    creamos controller, falta services y finalizar controller

---
+++ src/main/java/com/example/demo/model/controllers/ChatsController.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.controllers;
+
+import com.example.demo.model.DTOs.Chats.ChatMessageDTO;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatsController {
+
+}
